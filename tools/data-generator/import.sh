3721390f19bcfa7db0032a447a9a02d239f7a0b6
Andrei Palchys <andrei_palchys@epam.com>
2018-08-09 08:41:20 +0300

    fix: add more logs for understanding issue #1

--- tools/data-generator/import.sh
+++ tools/data-generator/import.sh
@@ -1,3 +1,6 @@
+echo "Available files:"
+ls /tmp/out
+
 mongoimport --host mongodb --port 27017 --db rsschool --jsonArray --mode upsert --type json --collection users /tmp/out/users.json
 mongoimport --host mongodb --port 27017 --db rsschool --jsonArray --mode upsert --type json --collection courses /tmp/out/courses.json
 mongoimport --host mongodb --port 27017 --db rsschool --jsonArray --mode upsert --type json --collection coursestudents /tmp/out/coursestudents.json
